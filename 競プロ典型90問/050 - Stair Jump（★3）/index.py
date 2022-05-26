44f43d60645fcb60bbc3e7428eb386a709cdd185
keyam <kei.329.yama@icloud.com>
2022-05-26 13:04:21 +0000

    2022/05/26

--- 競プロ典型90問/050 - Stair Jump（★3）/index.py
+++ 競プロ典型90問/050 - Stair Jump（★3）/index.py
@@ -1,13 +1,13 @@
-inputN, inputL = map(int, input().split())
-# dp[i] = 1 (i = 0)
-# dp[i] = dp[i - 1] (1 <= i < L)
-# dp[i] = dp[i - 1] + dp[i - L] (i >= L)
-dp = list(range(inputN + 1))
-dp[0] = 1
-for i in range(1, inputN + 1):
-    if i < inputL:
-        dp[i] = dp[i - 1]
-    else:
-        dp[i] = dp[i - 1] + dp[i - inputL]
+N, L = map(int, input().split())
 
-print(dp[inputN] % 1000000007)
+NUM = 1000000007
+
+dp = []
+dp.append(1)
+for i in range(1, N + 1):
+    if i < L:
+        dp.append(dp[i - 1])
+    else:
+        dp.append(dp[i - 1] + dp[i - L])
+
+print(dp[N] % NUM)
